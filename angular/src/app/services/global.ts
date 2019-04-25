22a16ff004d0029c2818c35a7a09ded0f06bbe23
Olivier Charpentier <olivier-charpentier@alain-bensoussan.com>
2019-04-25 10:01:43 +0200

    initial

---
+++ angular/src/app/services/global.ts
@@ -0,0 +1,4 @@
+export var GLOBAL = {
+// url: 'http://127.0.0.1/symfony/udemy1/symfony/web/app_dev.php'
+url: 'http://localhost:7080/test_angsymf/Task-Manager-with-Symfony-Angular4/symfony/web/app_dev.php'
+};
